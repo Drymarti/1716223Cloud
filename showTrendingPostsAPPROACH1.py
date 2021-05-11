23fcf0bfc9346d7157286b041258b38c6622462b
Marcin Ciepiel <marcin@reddeergames.com>
2021-05-11 02:23:08 +0100

    Added Files
    Added Files

---
+++ showTrendingPostsAPPROACH1.py
@@ -0,0 +1,43 @@
+#import tweepy libraries
+import tweepy 
+
+#Login access and secret keys of app from Twitter API
+access_token="1863328567-aKhvMhOmqvlamHyMeSlyq7to6s8xp2DsBgFpRX3"
+access_token_secret="y5127b1wA80U87SBnQGUD3jdHUDs4RtrTVD10YkgJbQ5T"
+
+api_key="jpzMRsUnewtcY4zINWX99bz9N"
+api_key_secret="tN92orzYxGlqhN6ex4SuJRqbw7KFXPVMjiMFOVA9rXRnj4auXH"
+
+#Authenticate software
+auth=tweepy.OAuthHandler(consumer_key=api_key, consumer_secret=api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+#Connect with Twitter API to our project
+api=tweepy.API(auth)
+print(api)
+
+india_woeid=23424848 #geo-localization set to India with India Code
+
+trend_result=api.trends_place(india_woeid) #Show trends from India only
+
+
+
+#Loop for receiving popular tweets. For top 10 popular tweets in India 
+for tweet in search_result[0]["popular"][:10]:
+    print(tweet["tweets"]) #Output the tweet
+    print(tweet["statuses"]) #Output of tweet's content and user
+    print(tweet["search_metadata"])  #More metadata about tweet
+    
+    
+#Loop for receiving popular tweets. For top 50 popular tweets in India 
+for tweet in search_result[0]["popular"][:50]:
+    print(tweet["tweets"]) #Output the tweet
+    print(tweet["statuses"]) #Output of tweet's content and user
+    print(tweet["search_metadata"])  #More metadata about tweet
+        
+#Loop for receiving popular tweets. For top 100 popular tweets in India 
+for tweet in search_result[0]["popular"][:100]:
+    print(tweet["tweets"]) #Output the tweet
+    print(tweet["statuses"]) #Output of tweet's content and user
+    print(tweet["search_metadata"])  #More metadata about tweet
+
